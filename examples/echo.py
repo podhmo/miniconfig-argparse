aca3ec825a5ee26e91de72d16640a53c4c8bed18
podhmo <podhmo+altair@beproud.jp>
2015-11-23 03:24:12 +0900

    examples

---
+++ examples/echo.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+import sys
+from miniconfig_argparse import Configurator
+import handofcats
+
+def includeme(config):
+    parser = config.parser
+    parser.add_argument("--without-echo", action="store_false", dest="echo")
+
+
+def main(argv):
+    config = Configurator()
+    config.include("miniconfig_argparse.configjson")
+    config.include(includeme)
+    args = config.make_args(argv)
+
+    print(args.echo)
+    if args.echo:
+        print("echo:")
+        print(args)
+
+if __name__ == "__main__":
+    # calling with --cli-input-json="file://./config.json"
+    argv = sys.argv[1:]
+    main(argv)
---
+++ examples/greeting.py
@@ -0,0 +1,30 @@
+# -*- coding:utf-8 -*-
+import logging
+logger = logging.getLogger(__name__)
+
+
+def greeting(message, is_surprised=False, name="foo"):
+    """ greeting message
+
+    :param message: message of greeting
+    :param is_surprised: surprised or not (default=False)
+    :param name: name of actor
+    """
+    logger.info("greeting start (name=%s)", name)
+    suffix = "!" if is_surprised else ""
+    print("{name}: {message}{suffix}".format(name=name, message=message, suffix=suffix))
+
+
+def includeme(config):
+    parser = config.make_parser(greeting)
+    config.replace_parser(parser)
+
+
+if __name__ == "__main__":
+    import sys
+    from miniconfig_argparse import get_configurator
+    config = get_configurator()
+    config.include(includeme)
+
+    args = config.make_args(sys.argv[1:])
+    config.call_function_as_command(greeting, args)
